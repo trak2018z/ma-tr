0f3235441d9f386b543002223c213de1e32c5acf
sylwekczmil <sylwekczmil@gmail.com>
2017-11-15 21:08:53 +0100

    add simple test

---
+++ src/test/kotlin/pl/sylwekczmil/ma/domain/mongo/dashboard/DashboardHandlerTest.kt
@@ -0,0 +1,18 @@
+package pl.sylwekczmil.ma.domain.mongo.dashboard
+
+import org.junit.Test
+import pl.sylwekczmil.ma.infrastructure.security.UserAccessorTestImpl
+import pl.sylwekczmil.ma.shared.OtherUserDataAccessException
+
+class DashboardHandlerTest {
+
+    val dashboardHandler: DashboardHandler = DashboardHandler(UserAccessorTestImpl())
+
+    @Test(expected = OtherUserDataAccessException::class)
+    fun `throw exception if other user want to change dashboard`() {
+        val dashboard = Dashboard()
+        dashboard.username = "other"
+        dashboardHandler.handleActions(dashboard)
+    }
+
+}
